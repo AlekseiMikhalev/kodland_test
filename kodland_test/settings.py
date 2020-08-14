6c2c72d7bccdcaa0d8d6ebebe7553f43d63f0912
Aleksei Mikhalev <aleks_mihalev@list.ru>
2020-08-15 02:57:48 +0300

    Update settings.py

--- kodland_test/settings.py
+++ kodland_test/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/images_folder/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
